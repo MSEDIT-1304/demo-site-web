046af480462c69aaa5c3df5524eb3dd76dc92835
M.S Édit <mairy.gillen@gmail.com>
2026-06-29 18:54:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def accueil():
     return render_template("index.html")
 
 
@@ -29,4 +29,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
